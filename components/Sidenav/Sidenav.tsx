b78c39f227972b66fc9f3be677a85835e0f8d77d
saurabhburade <sburade3@gmail.com>
2022-05-08 14:06:43 +0530

    Chore : Add home links

--- components/Sidenav/Sidenav.tsx
+++ components/Sidenav/Sidenav.tsx
@@ -7,22 +7,29 @@ import { RiVipCrown2Line } from 'react-icons/ri'
 import * as aiIcons from 'react-icons/ai'
 import * as biIcons from 'react-icons/bi'
 import * as bsIcons from 'react-icons/bs'
+import Link from 'next/link'
 
 function Sidenav() {
   return (
     <div className="sticky top-0 z-50 w-64 h-screen bg-gray-100 border ">
       <div className="flex justify-between px-5 pt-5 pb-5 w-100 nav-head">
-        <h2 className="text-2xl font-bold">Youtube</h2>
+        <Link href="/">
+          <p className="text-2xl font-bold cursor-pointer"> Youtube</p>
+        </Link>
         <HiMenuAlt1 size="2em" />
       </div>
       <div>
-        <NavItem>
-          {' '}
-          <div className="mr-5">
-            <biIcons.BiHomeAlt size="1.4em" />
+        <Link href="/">
+          <div>
+            <NavItem>
+              {' '}
+              <div className="mr-5">
+                <biIcons.BiHomeAlt size="1.4em" />
+              </div>
+              <p>Home</p>
+            </NavItem>
           </div>
-          <p>Home</p>
-        </NavItem>
+        </Link>
         <NavItem>
           {' '}
           <div className="mr-5">
